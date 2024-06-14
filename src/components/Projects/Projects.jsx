8a01fdd3f49bff2cbea4d2af23a6ab0cec91b4bf
KmiloLopez <andres.lopez2018@outlook.es>
2024-06-14 00:07:02 -0500

    setting up on the same page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import dataprojects from "./../../assets/images/projects/dataprojects.js";
+//import { dataprojects } from "./../../assets/images/projects/dataprojects.js";
 
 import {
   AdInfoDesc,
@@ -16,18 +16,29 @@ import {
   StyledParragraph,
 } from "./Projects.styled";
 import LinkToProject from "../linksprojects/LinkToProject";
-import imagendo from "../../assets/images/projects/ice2.png";
+import ice2 from "../../assets/images/projects/ice2.png";
+import ice2device from "../../assets/images/projects/device/icephone.png";
+import {
+  DirectLinksText,
+  GITtext,
+  GitIconLink,
+  IMGStyleGit,
+  SiteIconLink,
+  Sitetext,
+} from "../linksprojects/LinkToProject.Styled.jsx";
+import { SocialCircle, SocialMainContainer } from "../Social/Social.styled.jsx";
 
 // import FrontendIcon from "../icons/frontend/FrontendIcon";
 
 const Projects = () => {
-  const [selectedIndex, setSelectedIndex] = useState(null);
+  //const [selectedIndex, setSelectedIndex] = useState(null);
+  const [showFirstP, setShowFirstP] = useState(false);
 
-  const handleClick = (index) => {
-    setSelectedIndex(index);
+  const handleClick1 = () => {
+    setShowFirstP(!showFirstP);
   };
-  const handleHideClick = (index) => {
-    setSelectedIndex(null);
+  const handleHideClick1 = () => {
+    setShowFirstP(false);
   };
 
   return (
@@ -47,8 +58,65 @@ const Projects = () => {
           numquam ipsam, corporis, totam quae itaque odit placeat cumque aliquid
           rem unde eum inventore, nisi asperiores libero! Et, at sequi.
         </StyledParragraph>
-        <img src={imagendo} alt="dfasdf" />
 
+        <>
+          <ProjectContainer key={index}>
+            <RelativeContainer>
+              <ImgStyle onClick={() => handleClick1()} src={ice2} alt="some" />
+
+              <ImgStyleDevice
+                className="img-device"
+                src={ice2device}
+                alt="sea"
+              />
+              <ProjectTitle>IceCream Landing Page</ProjectTitle>
+            </RelativeContainer>
+          </ProjectContainer>
+          {showFirstP === true && (
+            <AdditionalContainer>
+              <AdInfoTitle>IceCream Landing Page</AdInfoTitle>
+              <AdInfoDesc>
+                Here you can find all the details related to this project.Here
+                you can find all the details related to this project.Here you
+                can find all the details related to this project.Here you can
+                find all the details related to this project.Here you can find
+                all the details related to this project.Here you can find all
+                the details related to this project.
+              </AdInfoDesc>
+
+              <StyledCloseIcon onClick={handleHideClick1} />
+
+              <DirectLinksText>Direct links:</DirectLinksText>
+              <SocialMainContainer>
+                <SocialCircle>
+                  <GitIconLink
+                    href="https://github.com/KmiloLopez/Proyect-IceCream"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IMGStyleGit />
+                    <GITtext className="git-text">Let's see the code</GITtext>
+                  </GitIconLink>
+                </SocialCircle>
+
+                <SocialCircle>
+                  <SiteIconLink
+                    href="https://jorgemomo.github.io/iceCream/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <IMGStyleWebSite />
+                    <Sitetext className="site-text">
+                      Let's check how it looks
+                    </Sitetext>
+                  </SiteIconLink>
+                </SocialCircle>
+              </SocialMainContainer>
+            </AdditionalContainer>
+          )}
+        </>
+
+        {/* 
         {dataprojects.map((item, index) => {
           return (
             <>
@@ -80,7 +148,7 @@ const Projects = () => {
               )}
             </>
           );
-        })}
+        })} */}
       </ProjectsMainContainer>
     </MyPojectsSection>
   );
